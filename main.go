a12bcc8bcc09d7239be8751b38646d6e06f3d54c
prem0x01 <mankars130@gmail.com>
2025-07-17 20:26:01 +0000

    Fix: Typo > to <

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ import (
 )
 
 func main() {
-	if len(os.Args) > 2 {
+	if len(os.Args) <= 2 {
 		fmt.Println(`Please provide a pdf file.
 			Ex= tpdf example.pdf`)
 		os.Exit(1)
@@ -18,7 +18,7 @@ func main() {
 	pdf := os.Args[2]
 	tpdf := "./output.txt"
 
-	err := utils.ConvertPdfToText(pdf, tpdf)
+	err := utils.ConvertPDFToText(pdf, tpdf)
 
 	if err != nil {
 		fmt.Println("[!] Error converting PDF:", err)
